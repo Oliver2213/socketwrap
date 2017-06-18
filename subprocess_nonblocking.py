6abb5da98cc5504e75eb80fe2f11e3ac0a6cbf0a
Blake Oliver <oliver22213@me.com>
2017-06-18 02:10:35 -0400

    Clarify the subprocess_nonblocking module by renaming the more cryptically-named variables to names that reflect their purpose and add docstrings and comments

--- subprocess_nonblocking.py
+++ subprocess_nonblocking.py
@@ -1,6 +1,7 @@
 # recipe-440554-1
 # This is from http://code.activestate.com/recipes/440554/
 
+# With modifications from Blake Oliver <oliver22213@me.com>
 
 import os
 import subprocess
@@ -14,7 +15,7 @@ if subprocess.mswindows:
     from win32file import ReadFile, WriteFile
     from win32pipe import PeekNamedPipe
     import msvcrt
-else:
+else: # unix-like OS
     import select
     import fcntl
 
@@ -23,12 +24,15 @@ class Popen(subprocess.Popen):
         return self._recv('stdout', maxsize)
     
     def recv_err(self, maxsize=None):
+        """Return up to maxsize characters from stderr."""
         return self._recv('stderr', maxsize)
 
     def send_recv(self, input='', maxsize=None):
+        """Send input, and receive up to maxsize chars from stdout and stderr. Return a tuple of the form (sent_chars, stdout_output, stderr_output)."""
         return self.send(input), self.recv(maxsize), self.recv_err(maxsize)
 
     def get_conn_maxsize(self, which, maxsize):
+        """Return a sane number for the maxsize parameter."""
         if maxsize is None:
             maxsize = 1024
         elif maxsize < 1:
@@ -40,7 +44,9 @@ class Popen(subprocess.Popen):
         setattr(self, which, None)
     
     if subprocess.mswindows:
+        # define the send method for windows
         def send(self, input):
+            """Send the given string to the subprocess's stdin stream. Return None if stdin was not directed to a pipe."""
             if not self.stdin:
                 return None
 
@@ -57,6 +63,7 @@ class Popen(subprocess.Popen):
             return written
 
         def _recv(self, which, maxsize):
+            """Return up to maxsize chars from eithe r'stdin' or 'stderr'."""
             conn, maxsize = self.get_conn_maxsize(which, maxsize)
             if conn is None:
                 return None
@@ -79,7 +86,7 @@ class Popen(subprocess.Popen):
                 read = self._translate_newlines(read)
             return read
 
-    else:
+    else: # define for unix-like OSs
         def send(self, input):
             if not self.stdin:
                 return None
@@ -120,35 +127,35 @@ class Popen(subprocess.Popen):
                 if not conn.closed:
                     fcntl.fcntl(conn, fcntl.F_SETFL, flags)
 
-message = "Other end disconnected!"
 
-def recv_some(p, t=.1, e=1, tr=5, stderr=0):
-    if tr < 1:
-        tr = 1
-    x = time.time()+t
-    y = []
-    r = ''
-    pr = p.recv
+def recv_some(subproc, timeout=0.1, throw_exception=True, tries=5, stderr=False):
+    if tries < 1:
+        tries = 1
+    x = time.time()+timeout
+    data = [] # a list of chunks of data we've received from the pipe
+    received = '' # string of characters we've received from the pipe
+    pipe_receive = subproc.recv
     if stderr:
-        pr = p.recv_err
-    while time.time() < x or r:
-        r = pr()
-        if r is None:
-            if e:
-                raise Exception(message)
-            else:
+        pipe_receive = subproc.recv_err
+    while time.time() < x or received:
+        received = pipe_receive()
+        if received == None: # if we didn't get any output
+            if throw_exception:
+                raise PipeClosedError
+            else: # do not raise an exception
                 break
-        elif r:
-            y.append(r)
-        else:
-            time.sleep(max((x-time.time())/tr, 0))
-    return ''.join(y)
+        elif received: # we did get some data
+            data.append(received)
+        else: # we probably got an empty string, so the pipe is open but there's nothing new
+            time.sleep(max((x-time.time())/tries, 0))
+    # return all of the data concatenated 
+    return ''.join(data)
     
-def send_all(p, data):
+def send_all(subproc, data):
     while len(data):
-        sent = p.send(data)
+        sent = subproc.send(data)
         if sent is None:
-            raise Exception(message)
+            raise PipeClosedError
         data = buffer(data, sent)
 
 if __name__ == '__main__':
@@ -163,5 +170,5 @@ if __name__ == '__main__':
         send_all(a, cmd + tail)
         print recv_some(a),
     send_all(a, 'exit' + tail)
-    print recv_some(a, e=0)
+    print recv_some(a, throw_exception=False)
     a.wait()
